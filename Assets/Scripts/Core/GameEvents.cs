be93e7f4ae0c648e2d4282bcbd8708e52e3ea051
aeonic-dev <io@aeonic.design>
2022-08-31 21:50:23 -0500

    Options, events and GameManager singleton

---
+++ Assets/Scripts/Core/GameEvents.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Contains events for core gameplay.
+/// </summary>
+public class GameEvents {
+    /// <summary>
+    /// Invoked when the game's options are saved.
+    /// </summary>
+    public readonly UnityEvent optionsReloadEvent = new UnityEvent();
+}
