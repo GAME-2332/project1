2719ba6fdd60699f0d55db1c889d9fee825b79da
aeonic-dev <io@aeonic.design>
2022-09-08 16:39:32 -0500

    Player movement rewrite

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,26 +4,59 @@ using UnityEngine;
 
 /// <summary>
 /// A component providing basic movement controls for the player based on configured inputs from the GameManager.
+/// Movement units are in units per second, thus are mostly divided by 50 (number of physics ticks per second).
 /// </summary>
 public class PlayerMovement : MonoBehaviour {
+    [Header("Physics")]
     public Transform playerLook;
-    public float bobbingIntensity;
-    public float bobbingSpeed;
-    public float bobbingIntensitySprinting;
-    public float bobbingSpeedSprinting;
-    public float bobbingIntensityCrouching;
-    public float bobbingSpeedCrouching;
-    public float mouseSensitivity;
+    [Tooltip("Base movement speed")]
     public float moveSpeed;
+    [Range(1,3)]
+    [Tooltip("Sprinting speed modifier")]
     public float sprintModifier;
+    [Range(0,1)]
+    [Tooltip("Crouching speed modifier")]
     public float crouchModifier;
+    [Tooltip("Jump speed")]
+    public float jumpSpeed;
+    [Tooltip("Acceleration due to gravity")]
+    public float fallSpeed;
+    [Header("Controls")]
+    public float mouseSensitivity;
+    [Range(.5f, 1)]
+    [Tooltip("The player's scale when crouching")]
     public float crouchHeight;
-    public float jumpForce;
+    [Header("Visuals")]
+    [Range(0, 1)]
+    [Tooltip("How much to smooth the crouching animation")]
+    public float crouchSmoothing;
+    [Tooltip("Distance of the focal point for view bobbing")]
+    public float focalDistance;
+    [Range(.0005f, .1f)]
+    [Tooltip("View bobbing intensity when walking normally")]
+    public float bobbingIntensity;
+    [Range(2, 36)]
+    [Tooltip("View bobbing speed when walking normally")]
+    public float bobbingSpeed;
+    [Range(.0005f, .1f)]
+    [Tooltip("View bobbing intensity when sprinting")]
+    public float bobbingIntensitySprinting;
+    [Range(2, 36)]
+    [Tooltip("View bobbing speed when sprinting")]
+    public float bobbingSpeedSprinting;
+    [Range(.0005f, .1f)]
+    [Tooltip("View bobbing intensity when crouching")]
+    public float bobbingIntensityCrouching;
+    [Range(2, 36)]
+    [Tooltip("View bobbing speed when crouching")]
+    public float bobbingSpeedCrouching;
 
     private GameOptions gameOptions;
-    new private Rigidbody rigidbody;
-    private Vector3 playerLookZero;
-    private Vector3 crouchingScale;
+    private CharacterController controller;
+    private Vector3 cameraFocus;
+    private Vector3 initialScale;
+    private Vector3 initialPlayerLook;
+    private Vector3 velocity = Vector3.zero;
     private float xRot = 0;
     private float yRot = 0;
     private PlayerState playerState = PlayerState.Normal;
@@ -32,9 +65,9 @@ public class PlayerMovement : MonoBehaviour {
 
     void Start() {
         gameOptions = GameManager.instance.gameOptions;
-        rigidbody = GetComponent<Rigidbody>();
-        playerLookZero = playerLook.localPosition;
-        crouchingScale = new Vector3(1, crouchHeight, 1);
+        controller = GetComponent<CharacterController>();
+        initialScale = transform.localScale;
+        initialPlayerLook = playerLook.localPosition;
         groundDistance = GetComponent<Collider>().bounds.extents.y;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -55,28 +88,42 @@ public class PlayerMovement : MonoBehaviour {
             else playerState = PlayerState.Normal;
         }
 
-        // Apply player movement force
-        rigidbody.AddForce(GetInputVector() * MoveSpeed(), ForceMode.Force);
-        // Max speed to avoid crazy acceleration with AddForce weirdness
-        rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, 5f);
+        // Add player movement velocity
+        Vector3 horizontalVelocity = (GetInputVector() * MoveSpeed() / 50);
+        velocity.Set(horizontalVelocity.x, velocity.y, horizontalVelocity.z);
 
         // Crouching
-        if (playerState == PlayerState.Crouching) {
-            if (!transform.localScale.Equals(crouchingScale)) {
-                transform.localScale = Vector3.Lerp(transform.localScale, crouchingScale, .7f);
-                rigidbody.AddForce(Vector3.down * moveSpeed, ForceMode.Force);
-            }
-        } else transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, .3f);
+        float targetHeight = playerState == PlayerState.Crouching ? crouchHeight : initialScale.y;
+        if (transform.localScale.y != targetHeight) {
+            float deltaHeight = (transform.localScale.y - targetHeight) * (crouchSmoothing - 1);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + deltaHeight, transform.localScale.z);
+            controller.Move(Vector3.up * deltaHeight);
+        }
 
         // Jumping
         if (gameOptions.jump.GetKey() && isOnGround) {
             isOnGround = false;
-            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            velocity.y = jumpSpeed / 50;
         }
         
-        // Apply view bobbing if the player is moving fast enough horizontally
-        float groundSpeed = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z).magnitude;
-        if (isOnGround && groundSpeed > .5f) {
+        // Gravity
+        velocity.y -= fallSpeed / 50;
+        controller.Move(velocity);
+
+        // Calculate rotation from mouse movement
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity * gameOptions.mouseSensitivity.Value;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity * gameOptions.mouseSensitivity.Value;
+        yRot += mouseX;
+        xRot = Mathf.Clamp(xRot - mouseY, -90, 90);
+
+        // Rotate the player along only the horizontal axis
+        transform.rotation = Quaternion.Euler(0, yRot, 0);
+
+        // Rotate the player's look vector along both axes (the camera inherits this transformation)
+        playerLook.rotation = Quaternion.Euler(xRot, yRot, 0);
+        
+        // Apply view bobbing if the player is moving horizontally
+        if (isOnGround && horizontalVelocity.magnitude > 0) {
             Vector3 viewBobbing = playerState switch {
                 PlayerState.Crouching => ViewBobbing(bobbingSpeedCrouching, bobbingIntensityCrouching),
                 PlayerState.Sprinting => ViewBobbing(bobbingSpeedSprinting, bobbingIntensitySprinting),
@@ -85,31 +132,28 @@ public class PlayerMovement : MonoBehaviour {
             // Add the actual vector to the Player Look object's position
             playerLook.localPosition += viewBobbing;
         }
+
+        // Calculate focal point
+        Vector3 origin = playerLook.position - playerLook.localPosition + initialPlayerLook;
+        // RaycastHit hit;
+        // if (Physics.Raycast(origin, playerLook.forward, out hit, focalDistance)) cameraFocus = hit.point;
+        /*else */cameraFocus = origin + playerLook.forward * focalDistance;
+
+        // Compensate for bobbing offset
+        playerLook.LookAt(cameraFocus, Vector3.up);
     }
 
     void Update() {
-        // Calculate rotation/looking around in Update, every frame
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * mouseSensitivity * gameOptions.mouseSensitivity.Value;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * mouseSensitivity * gameOptions.mouseSensitivity.Value;
-
-        yRot += mouseX;
-        xRot = Mathf.Clamp(xRot - mouseY, -90, 90);
-
-        // Rotate the player along only the horizontal axis
-        transform.rotation = Quaternion.Euler(0, yRot, 0);
-        // Rotate the player's look vector along both axes (the camera inherits this transformation)
-        playerLook.rotation = Quaternion.Euler(xRot, yRot, 0);
-
         // Slowly reset the camera's position after bobbing
-        if (playerLook.localPosition != playerLookZero) {
-            playerLook.localPosition = Vector3.Lerp(playerLook.localPosition, playerLookZero, Time.deltaTime * 3);
+        if (playerLook.localPosition != initialPlayerLook) {
+            playerLook.localPosition = Vector3.Lerp(playerLook.localPosition, initialPlayerLook, Time.deltaTime * 3);
         }
     }
 
     private Vector3 ViewBobbing(float freq, float amp) {
         // Calculate view bobbing offset
         Vector3 vec = Vector3.zero;
-        vec.x += (Mathf.Cos(Time.time * freq / 2f)) * amp * 1.3f;
+        vec.x += (Mathf.Cos(Time.time * freq / 2f)) * amp * 1.2f;
         vec.y += (Mathf.Sin(Time.time * freq)) * amp;
         return vec;
     }
