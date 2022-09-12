cfd29843eb3c9293ce106e5507cfbcb84f012cd0
aeonic <io@aeonic.design>
2022-09-12 14:59:45 -0500

    Add SaveState info file, cleanup

--- Assets/Scripts/Persistence/SaveState.cs
+++ Assets/Scripts/Persistence/SaveState.cs
@@ -1,4 +1,6 @@
+using Microsoft.VisualBasic;
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,7 +9,12 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class SaveState {
     private const int startScene = 1;
-    private readonly int slot;
+
+    public readonly int slot;
+    public string lastSceneName;
+    public string lastSaveDate;
+    public string lastSaveTime;
+
     private SceneData currentScene;
     private string playerData;
     private string spawnPoint;
@@ -18,6 +25,7 @@ public class SaveState {
     /// <param name="slot"></param>
     public SaveState(int slot) {
         this.slot = slot;
+        ReadSceneInfo();
     }
 
     /// <summary>
@@ -33,7 +41,7 @@ public class SaveState {
     /// <summary>
     /// Loads the given scene by path or build index, serializing the active one and deserializing the new one.
     /// </summary>
-    public void LoadScene(string scenePath = null, int sceneIndex = -1, string spawnPoint = null) {
+    internal void LoadScene(string scenePath = null, int sceneIndex = -1, string spawnPoint = null) {
         // If another game scene is loaded (not the menu), write it to disk first
         playerData = null;
         this.spawnPoint = spawnPoint;
@@ -44,6 +52,7 @@ public class SaveState {
                 Directory.CreateDirectory(Path());
                 playerData = JsonUtility.ToJson(player.playerData);
                 File.WriteAllText(Path() + "/player.dat", playerData);
+                WriteSceneInfo();
             }
         }
 
@@ -56,29 +65,48 @@ public class SaveState {
     /// We need to listen for the new scene being loaded separately because SceneManager#LoadScene queues the new scene to load in the following frame.
     /// We can't just use Update because this class isn't a MonoBehaviour.
     /// </summary>
-    public void OnSceneLoaded(Scene old, Scene current) {
+    internal void OnSceneLoaded(Scene old, Scene current) {
         // Set up the newly loaded scene
         currentScene = SceneData.Get(SceneManager.GetActiveScene());
         if (currentScene == null) return; // Edge case in case we caught a scene load before SaveState#Load was called
         currentScene.Read(Path());
         currentScene.SpawnPlayer(playerData, spawnPoint);
-
-        // Save the now-current scene index to disk
-        Directory.CreateDirectory(Path());
-        File.WriteAllText(Path() + "/scene.dat", SceneManager.GetActiveScene().buildIndex.ToString());
-
         playerData = spawnPoint = null;
     }
 
     /// <summary>
     /// Called to serialize the current scene without loading another one; ie when the application is closed without switching scenes first.
     /// </summary>
-    public void SaveCurrent() {
+    internal void SaveCurrent() {
+        Directory.CreateDirectory(Path());
         if (currentScene != null) currentScene.Write(Path());
         var player = GameObject.FindObjectOfType<PlayerMovement>();
+        if (player != null) {
+            File.WriteAllText(Path() + "/player.dat", JsonUtility.ToJson(player.playerData));
+            WriteSceneInfo();
+        }
+    }
+
+    private void WriteSceneInfo() {
         Directory.CreateDirectory(Path());
-        if (player != null) File.WriteAllText(Path() + "/player.dat", JsonUtility.ToJson(player.playerData));
         File.WriteAllText(Path() + "/scene.dat", SceneManager.GetActiveScene().buildIndex.ToString());
+        lastSceneName = Regex.Replace(SceneManager.GetActiveScene().name, @"(\B[A-Z]+?(?=[A-Z][^A-Z])|\B[A-Z]+?(?=[^A-Z]))", " $1");
+        lastSaveDate = System.DateTime.Now.ToString("yyyy-MM-dd");
+        lastSaveTime = System.DateTime.Now.ToString("HH:mm:ss");
+        File.WriteAllLines(Path() + "/info.dat", new string[] { lastSceneName, lastSaveDate, lastSaveTime });
+    }
+
+    private void ReadSceneInfo() {
+        if (File.Exists(Path() + "/info.dat")) {
+            var lines = File.ReadAllLines(Path() + "/info.dat");
+            lastSceneName = lines[0];
+            lastSaveDate = lines[1];
+            lastSaveTime = lines[2];
+        }
+    }
+
+    public bool Exists() {
+        return lastSaveDate != null && lastSaveTime != null;
     }
 
     /// <summary>
