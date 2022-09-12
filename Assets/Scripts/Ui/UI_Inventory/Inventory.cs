91c87c5fa108245ab56f9b34b4b21bf36394ec97
awetos <78708899+awetos@users.noreply.github.com>
2022-09-11 23:16:24 -0500

    Loading Items
    Loads development test items into the inventory.

--- Assets/Scripts/Ui/UI_Inventory/Inventory.cs
+++ Assets/Scripts/Ui/UI_Inventory/Inventory.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
 
     List<GameObject> _Inventory;
+    List<GameObject> _Spaces;
+
+    [SerializeField]
+    GameObject DevTestItem;
+    [SerializeField]
+    GameObject DevTestItem2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,20 +23,36 @@ public class Inventory : MonoBehaviour
 
     public void LoadInventory()
     {
-       //Get the game manager, get the list of game objects. 
+        _Inventory = new List<GameObject>();
+        _Inventory.Add(DevTestItem);
+        _Inventory.Add(DevTestItem2);
+
+        //Get the game manager, get the list of game objects. 
+        //_Inventory = GameManager.instance.saveState.GetInventory()
+        LoadSpaces();
+
+        int itemCount = 0;
+        foreach (GameObject i in _Inventory)
+        {
+           
+            GameObject temp_go = Instantiate(i, _Spaces[itemCount].transform);
+           // temp_go.transform.SetParent(_Spaces[itemCount].transform);
+            itemCount++;
+        }
     }
 
-    public void PopulatePage()
+    void LoadSpaces()
     {
-        if(_Inventory != null)
+        _Spaces = new List<GameObject>();
+        GameObject _inventorySpace = GameObject.Find("Inventory Space");
+        for (int i = 0; i < _inventorySpace.transform.childCount; i++)
         {
-            //populate page
+            _Spaces.Add(_inventorySpace.transform.GetChild(i).gameObject);
+            Debug.Log("A child was added #" + i);
         }
-        else
-        {
-            LoadInventory();
-        }
+        return;
     }
+
 
     private void Update()
     {
