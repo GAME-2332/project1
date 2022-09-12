3d7e4ba887a71c02be35ee26fd1e63954b4511bf
aeonic <io@aeonic.design>
2022-09-12 12:39:53 -0500

    Use struct for PlayerData, fix PlayerMovement refs

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -6,30 +6,10 @@ using UnityEngine;
 /// Separate from PlayerMovement to avoid passing references back and forth.
 /// </summary>
 [Serializable]
-public class PlayerData : MonoBehaviour, ISerializationCallbackReceiver {
+public struct PlayerData {
     public Vector3 velocity;
     public float xRot;
     public float yRot;
     public PlayerMovement.PlayerState playerState;
     public bool isOnGround;
-
-    private PlayerMovement playerMovement;
-
-    public void OnBeforeSerialize() {
-        playerMovement = GetComponent<PlayerMovement>();
-        velocity = playerMovement.velocity;
-        xRot = playerMovement.xRot;
-        yRot = playerMovement.yRot;
-        playerState = playerMovement.playerState;
-        isOnGround = playerMovement.isOnGround;
-    }
-
-    public void OnAfterDeserialize() {
-        playerMovement = GetComponent<PlayerMovement>();
-        playerMovement.velocity = velocity;
-        playerMovement.xRot = xRot;
-        playerMovement.yRot = yRot;
-        playerMovement.playerState = playerState;
-        playerMovement.isOnGround = isOnGround;
-    }
 }
