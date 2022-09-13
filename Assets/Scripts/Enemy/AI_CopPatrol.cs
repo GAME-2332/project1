65cc5c2396c4270781e3da85911aeb21d4d944ee
akibIM <akib1994@gmail.com>
2022-09-13 16:43:50 -0500

    AI MallCop Update 2
    Problem fixed, Move to next position works, CurrentPosition is not constantly changing. AI agent follows player inside minDistance and goes back to patrol after player gets out of minDistance

--- Assets/Scripts/Enemy/AI_CopPatrol.cs
+++ Assets/Scripts/Enemy/AI_CopPatrol.cs
@@ -10,17 +10,20 @@ public class AI_CopPatrol : MonoBehaviour
     public UnityEngine.AI.NavMeshAgent MallCop;
 
     public Transform[] PatrolTarget;
-    public int CurrentPosition;
+    public int CurrentPosition = 0;
 
-    public NavMeshAgent AIAgent;
+    private NavMeshAgent AIAgent;
 
-    public float Speed;
     // Start is called before the first frame update
     void Start()
     {
+        AIAgent = GetComponent<NavMeshAgent>();
+
         CurrentPosition = 0;
 
         AIAgent.autoBraking = false;
+
+        GotToNextPoint();
     }
 
     // Update is called once per frame
@@ -35,24 +38,28 @@ public class AI_CopPatrol : MonoBehaviour
         {
             return;
         }
-        
-        if (Vector3.Distance(transform.position, (PatrolTarget[CurrentPosition].position)) < .5)
-        {
+
+        //if(transform.position != PatrolTarget[CurrentPosition].position)
+        /*if (
+               transform.position != PatrolTarget[CurrentPosition].position 
+               //&& Vector3.Distance(transform.position, (PatrolTarget[CurrentPosition].position)) < .5
+           )
+        {*/
             //AIAgent.destination = PatrolTarget[CurrentPosition].position;
-            MallCop.SetDestination(PatrolTarget[CurrentPosition].position);
-        }
-        else 
-        {
+            AIAgent.destination = PatrolTarget[CurrentPosition].position;
+        //}
+        //else 
+        //{
             CurrentPosition = (CurrentPosition + 1) % PatrolTarget.Length;
-        }
+        //}
     }
 
     public void Patrol()
     {
-        //if (AIAgent.remainingDistance < 1.0f)
-        //{ 
+        if (!AIAgent.pathPending && AIAgent.remainingDistance < 1.0f)
+        { 
             GotToNextPoint();
-        //}
+        }
 
         Debug.Log("Patrol works");
     }
--- Assets/Scripts/Enemy/PatrolwithoutNav.cs
+++ Assets/Scripts/Enemy/PatrolwithoutNav.cs
@@ -5,7 +5,48 @@ using UnityEngine.AI;
 
 public class PatrolwithoutNav : MonoBehaviour
 {
-    public Transform[] PatrolTarget;
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+
+    /*public Transform[] PatrolTarget;
     int CurrentPosition;
 
     public float Speed;
@@ -32,5 +73,5 @@ public class PatrolwithoutNav : MonoBehaviour
         {
             CurrentPosition = (CurrentPosition + 1) % PatrolTarget.Length;
         }
-    }
+    }*/
 }
