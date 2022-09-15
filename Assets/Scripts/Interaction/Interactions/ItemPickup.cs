54751d98c0df437c09a471c46ff157fab5c1df6c
aeonic <io@aeonic.design>
2022-09-15 12:47:13 -0500

    ItemPickup's item ItemInfo -> SOItemInfo

--- Assets/Scripts/Interaction/Interactions/ItemPickup.cs
+++ Assets/Scripts/Interaction/Interactions/ItemPickup.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(SaveMe))]
 public class ItemPickup : Interactible {
-    public ItemInfo item;
+    public SOItemInfo item;
 
     public override void Interact() {
         GameManager.instance.saveState.heldItems.Add(item);
