c90429abeeab675e85e804f11c7a16dfb26865b4
aeonic <io@aeonic.design>
2022-09-19 19:29:17 -0500

    Create DialogueTree scriptable object

---
+++ Assets/Scripts/NPC/Dialogue/DialogueTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Describes a dialogue interaction tree.
+/// </summary>
+[Serializable]
+[CreateAssetMenu(fileName = "New Dialogue Tree", menuName = "Dialogue Tree", order = 0)]
+public class DialogueTree : ScriptableObject {
+    public DialogueNode entryPoint;
+
+    public DialogueContext Traverse(Action<string[]> refreshOptions, Action onDialogueEnd) {
+        return new DialogueContext(entryPoint, refreshOptions, onDialogueEnd);
+    }
+}
