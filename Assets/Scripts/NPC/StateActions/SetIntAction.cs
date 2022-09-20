8706edba4e9918380633de282eb6e99a06a880ba
aeonic <io@aeonic.design>
2022-09-19 19:24:22 -0500

    Create IStateAction and implementations under StateActions

---
+++ Assets/Scripts/NPC/StateActions/SetIntAction.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[Serializable]
+public class SetIntAction : SetGlobalsAction<int> {
+    public override void Execute() {
+        GameManager.instance.saveState.globals.SetInt(variableName, value);
+    }
+}
