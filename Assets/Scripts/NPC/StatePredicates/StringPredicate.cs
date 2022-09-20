936e62f74f42dae440d7be5d18f20bc2c50d32f4
aeonic <io@aeonic.design>
2022-09-19 19:25:01 -0500

    Create IStatePredicate and implementations under StatePredicates

---
+++ Assets/Scripts/NPC/StatePredicates/StringPredicate.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+[Serializable]
+public class StringPredicate : GlobalsPredicate<string> {
+    public override string GetValue(string name) => GameManager.instance.saveState.globals.GetString(name);
+}
