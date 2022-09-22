67d940d526966e49e2c9363b307716b05237e220
clementkirton <clementkirton@gmail.com>
2022-09-22 12:56:48 -0500

    Ui
    +Added open/close function to open canvas ui

---
+++ Assets/Scripts/NPC/MenuToggle.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+ using System.Collections;
+ 
+ public class MenuToggle : MonoBehaviour {
+     private Canvas CanvasObject; // Assign in inspector
+ 
+     void Start()
+     {
+         CanvasObject = GetComponent<Canvas> ();
+     }
+ 
+     void Update() 
+     {
+         if (Input.GetKeyUp(KeyCode.E)) {
+            GameManager.instance.gameState = CanvasObject.enabled ? GameManager.GameState.Playing : GameManager.GameState.Dialogue;
+            CanvasObject.enabled = !CanvasObject.enabled;
+         }
+     }
+ }
--- Assets/Scripts/NPC/NPC test.cs
+++ Assets/Scripts/NPC/NPC test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine TMPro;
+using TMPro;
 
 public class NPCTest : Interactible {
     public GameObject spritePrefab;
