d2769fbd35dcf6b525ea68eb97e97e9ed02882bd
aeonic <io@aeonic.design>
2022-09-22 17:54:44 -0500

    Fix (actually, finally fix) inventory serialization

--- Assets/Scripts/Core/ItemRegistry.cs
+++ Assets/Scripts/Core/ItemRegistry.cs
@@ -1,8 +1,12 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 public class ItemRegistry {
     private static Dictionary<string, SOItemInfo> items = Util.LoadResources<SOItemInfo>("ItemSOs");
 
     public static SOItemInfo GetItem(string name) => items[name];
-    public static void Wake() { }
+
+    public static void Wake() {
+        Debug.Log(items);
+    }
 }
