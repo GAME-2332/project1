7fa6a744f39f5cc820f5c6f7f7dfd3da14f89072
aeonic <io@aeonic.design>
2022-10-04 11:24:26 -0500

    Add modes to global int predicate and action

--- Assets/Scripts/NPC/StateActions/SetIntAction.cs
+++ Assets/Scripts/NPC/StateActions/SetIntAction.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.ComponentModel;
 
 [Serializable]
 public class SetIntAction : SetGlobalsAction<int> {
+    [DefaultValue(Mode.Set)]
+    public Mode mode;
+    
     public override void Execute() {
-        GameManager.instance.saveState.globals.SetInt(variableName, value);
+        GameManager.instance.saveState.globals.SetInt(variableName, mode switch {
+            Mode.Add => GameManager.instance.saveState.globals.GetInt(variableName) + value,
+            Mode.Subtract => GameManager.instance.saveState.globals.GetInt(variableName) - value,
+            _ => value
+        });
+    }
+
+    public enum Mode {
+        Set, Add, Subtract
     }
 }
--- Assets/Scripts/NPC/StatePredicates/CheckIntPredicate.cs
+++ Assets/Scripts/NPC/StatePredicates/CheckIntPredicate.cs
@@ -1,6 +1,31 @@
 ﻿using System;
+using System.ComponentModel;
 
 [Serializable]
 public class CheckIntPredicate : CheckGlobalsPredicate<int> {
+    [DefaultValue(Mode.Equal)]
+    public Mode mode;
+    
     public override int GetValue(string name) => GameManager.instance.saveState.globals.GetInt(name);
+
+    public override bool Check() {
+        int check = GetValue(variableName);
+        return mode switch {
+            Mode.NotEqual => check != checkValue,
+            Mode.GreaterThan => check > checkValue,
+            Mode.GreaterThanOrEqual => check >= checkValue,
+            Mode.LessThan => check < checkValue,
+            Mode.LessThanOrEqual => check <= checkValue,
+            _ => check == checkValue
+        };
+    }
+
+    public enum Mode {
+        Equal,
+        NotEqual,
+        GreaterThan,
+        GreaterThanOrEqual,
+        LessThan,
+        LessThanOrEqual
+    }
 }
