ab19405cdd68ef3be423d9303a71a97150839763
aeonic <io@aeonic.design>
2022-10-13 13:09:24 -0500

    Carousel fix

--- Assets/Carousel.cs
+++ Assets/Carousel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -7,11 +8,9 @@ public class Carousel : MonoBehaviour {
     // I don't even know anymore
     public Transform pivot;
     public int speed;
-    [SerializeReference] [PickImpl(typeof(IStatePredicate))]
-    public IStatePredicate condition;
 
     void Update() {
-        if (condition.Check()) {
+        if (GameManager.instance.saveState.globals.GetInt("power_boxes_fixed") >= 3) {
             transform.RotateAround(pivot.position, transform.up, speed * Time.deltaTime);
         }
     }
